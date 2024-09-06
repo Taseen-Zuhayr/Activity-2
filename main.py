ea638cfb8ccd6efc9c4a32f2c3815b5ef00c0c88
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-09-06 23:22:18 +0600

    Activity 2

---
+++ main.py
@@ -0,0 +1,12 @@
+name = "Codingal"
+age = 4
+rating = 9.5
+education = True
+
+print(type(name))
+print(type(age))
+print(type(rating))
+print(type(education))
+
+rating = int(rating)
+print(rating)
